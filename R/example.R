2680e7418b69b7a4c00764fadc2ee5cd23794d59
aljers <aljers@outlook.com>
2022-12-18 00:28:01 -0500

    updated

--- R/example.R
+++ R/example.R
@@ -10,7 +10,14 @@ true_beta <- full_fit$beta
 true_sigmasq <- full_fit$sigmasq
 
 boot <- bootstrap(epilepsy, subject_index = 1, B = 100, true_beta, true_sigmasq)
-boot %>% group_by(term) %>% summarise(estimates = mean(estimate),
+BS_summary <- boot %>% group_by(term) %>% summarise(estimates = mean(estimate),
                                       se = sd(estimate),
                                       LowerCI = quantile(estimate,0.025),
                                       UpperCI = quantile(estimate,0.975))
+## hypothesis test
+if(0>BS_summary[4,]$LowerCI & 0<BS_summary[4,]$UpperCI){
+  print('0 is inside the 95% Confidence Interval, so we do not reject H0, and beta is not significantly different from 0')
+}
+else{
+  print('0 is outside the 95% Confidence Interval, so we reject H0, and beta is significantly different from 0')
+}
