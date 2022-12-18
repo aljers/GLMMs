ed6d2fc3427f6470fc347188b442685f0ac1d5ef
aljers <aljers@outlook.com>
2022-12-17 22:25:41 -0500

    replace the for loop by matrix multiplication, efficiency get increased

--- R/poisson_glmm.R
+++ R/poisson_glmm.R
@@ -1,14 +1,9 @@
 library(tidyverse)
 library(dplyr)
-library(here)
 library(lme4)
+source("estimation_functions.R")
 
 ## Import and mutate the dataset:
-epilepsy <- read_csv("epilepsy.csv")
-epilepsy <- epilepsy %>%
-  group_by(id, treat, expind, age) %>%
-  summarize(seizures = sum(seizures),
-            .groups = "drop")
 
 
 ## Conduct bootstrap:
@@ -16,39 +11,25 @@ epilepsy <- epilepsy %>%
 ## attributes within each id.
 ## Repeat this for N times.
 
-bootstrap <- function(N = 100){
-  source(here("estimation_functions.R"))
-  beta <- list()
-  sigmasq <- c()
-  for(i in 1:N){
-    ## Resample 30 persons with `treat == 1`:
-    boot.treat <- epilepsy %>%
-      filter(treat == 1)
-    boot.index1 <- sample(unique(boot.treat$id), 30, replace = TRUE)
-    boot.treat <- do.call(rbind, lapply(boot.index1, function(x) epilepsy[epilepsy$id == x,]))
 
-    ## Resample 29 persons with `treat == 0`:
-    boot.placebo <- epilepsy %>%
-      filter(treat == 0)
-    boot.index0 <- sample(unique(boot.placebo$id), 29, replace = TRUE)
-    boot.placebo <- do.call(rbind, lapply(boot.index0, function(x) epilepsy[epilepsy$id == x,]))
+bootstrap <- function(dataset, subject_index = 1, B = 100, true_beta, true_sigmasq){
 
-    boot <- rbind(boot.treat, boot.placebo)
-    ## Fit the model:
-    mod <- run_model(boot, "epilepsy")
-    betai <- mod$beta
-    sigmasqi <- mod$sigmasq
-    sigmasq <- c(sigmasq, sigmasqi)
-    beta[[i]] <- betai
-  }
-  return(list(beta = beta,
-              sigmasq = sigmasq))
+  n_sub <- nrow(unique(dataset %>% select(subject_index)))
+  n_rep <- nrow(dataset)/n_sub
+  z_i <- rep(rnorm(B*n_sub,0,sqrt(true_sigmasq)),each=2)
+  mu_sample <- exp(rep(as.matrix(dataset  %>%
+                                   mutate(intercept = rep(1,n()),
+                                          interact = expind*treat) %>%
+                                   select(intercept, age, expind, interact) ) %*%
+                         true_beta, B)+z_i)
+  data_sample <- dataset %>%
+    select(-seizures) %>%
+    slice(rep(1:n(), B)) %>%
+    mutate(seizures = rpois(length(mu_sample),mu_sample),
+           Boot = rep(1:B,each = nrow(dataset)))
+  data_sample %>%
+    group_by(Boot) %>%
+    summarise(tidy(glmer(seizures ~ age + expind + expind:treat + (1|id), family = poisson)),
+              .groups = 'drop')
 }
-
-
-## Test the function:
-boot <- bootstrap()
-plot(density(boot$sigmasq))
-
-
 
