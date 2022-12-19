49953540cbacf50f79ead1c95e848082b66ed84a
aljers <aljers@outlook.com>
2022-12-18 23:45:13 -0500

    change the method of hypothesis

--- R/example.R
+++ R/example.R
@@ -16,9 +16,18 @@ BS_summary <- BootSummary(boot)
 h0_beta <- true_beta
 h0_beta[4] <- 0
 boot_h0 <- bootstrap(dataset = epilepsy, h0_beta, true_sigmasq)
-h0_summary <- BootSummary(boot_h0)
-if(0>h0_summary[4,]$LowerCI & 0<h0_summary[4,]$UpperCI){
-  print('0 is inside the 95% Confidence Interval, so we do not reject H0, and beta is not significantly different from 0')
+## Compute the proportion of time that we obtain test statistic larger(more
+## extreme) then the t value obtained from original model fit, full_fit. We use
+## this proportion as the p-value of the significance test.
+t_value <- full_fit$test_stat[4]
+p_value <- (boot_h0 %>%
+  filter(term == "expind:treat") %>%
+  summarize(p = mean(abs(statistic) > abs(t_value))))$p
+
+if(p_value < 0.05){
+  print('Given 5% significance level, p value < 0.05 shows enough evidence against the null hypothesis.
+        We reject H0, and beta is significantly different from 0')
 } else{
-  print('0 is outside the 95% Confidence Interval, so we reject H0, and beta is significantly different from 0')
+  print('Given 5% significance level, p value > 0.05 does not show enough evidence against the null hypothesis.
+        We do not reject H0, and beta is not significantly different from 0')
   }
--- R/setup.R
+++ R/setup.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(usethis)
 library(lme4)
 library(broom.mixed)
+
 epilepsy <-  read.csv('epilepsy.csv')
 epilepsy <- epilepsy %>%
   group_by(id, treat, expind, age) %>%
