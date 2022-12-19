a56ac2acf0706a70bf31461994ab79df2d0af183
yangzixuan650 <yangsharon650@gmail.com>
2022-12-18 20:28:17 -0500

    Update.

--- R/bootstrap_summary.R
+++ R/bootstrap_summary.R
@@ -1,3 +1,12 @@
+#' Summary of the bootstrap
+#'
+#' @param object a dataframe returned by bootstrap function
+#' @param SignifLv numeric: significance level, defaulted to 0.05
+#'
+#' @return a dataframe with the mean, se, and upper/lower quantile of the estimators
+#' @export
+#'
+#' @examples
 BootSummary <- function(object, SignifLv = 0.05){
   collection <- object %>% group_by(term) %>% summarise(estimates = mean(estimate),
                                                       se = sd(estimate),
